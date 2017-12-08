e6a3735be954e1ffac492424fd031912b2584911
dinesh <dineshreddy929@gmail.com>
2017-12-08 13:20:03 +0530

    openstack project list

--- export.sh
+++ export.sh
@@ -9,14 +9,27 @@ if [ $? -eq 0 ]; then
 else
    openstack service create \
   --name keystone --description "OpenStack Identity" identity
-  
+
    openstack endpoint create --region RegionOne \
   identity public http://controller:5000/v3
-  
+
    openstack endpoint create --region RegionOne \
   identity internal http://controller:5000/v3
 
    openstack endpoint create --region RegionOne \
   identity admin http://controller:35357/v3
+fi
+openstack domain list | grep -i domain
+if [  $? -eq 0 ]; then
+   echo "domain already exists"
+else
+   openstack domain create --description "Default Domain" default
+fi
 
+openstack project list | grep -i project
+if [  $? -eq 0 ]; then
+   echo "admin project already exists"
+else
+   openstack project create --domain default \
+  --description "Admin Project" admin
 fi
